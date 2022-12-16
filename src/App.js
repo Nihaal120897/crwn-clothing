ed74e4d13c58343b04d32b40cf99c857f3c32258
Nihaal120897 <nihaal.ramdass@rapp.com>
2022-12-16 14:28:39 +0400

    integrated firebase into application, create sign in page with sign in component and sign in with google

--- src/App.js
+++ src/App.js
@@ -1,29 +1,50 @@
 import React from "react";
 
-import { Route, Routes } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import Header from "./components/header/header.component";
+import { auth } from "./firebase/firebase.utils";
 
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
+class App extends React.Component {
+  constructor() {
+    super();
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Routes>
-        <Route exact path="/" element={<HomePage />} />
-        <Route path="/shop" element={<ShopPage />} />
-      </Routes>
-    </div>
-  );
+    this.state = {
+      currentUser: null,
+    };
+  }
+
+  unsubcribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubcribeFromAuth = auth.onAuthStateChanged((user) => {
+      this.setState({ currentUser: user });
+
+      console.log(user);
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubcribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header currentUser={this.state.currentUser} />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route path="/signin" component={SignInAndSignUpPage} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,24 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const config = {
+  apiKey: "AIzaSyAqsC3fFjg0eu8wf01bplCFjnDHq8WrIEE",
+  authDomain: "crown-db-cee56.firebaseapp.com",
+  projectId: "crown-db-cee56",
+  storageBucket: "crown-db-cee56.appspot.com",
+  messagingSenderId: "890173182569",
+  appId: "1:890173182569:web:fa38291aa09bfaa5f408f8",
+  measurementId: "G-5CJ6KGT5WR",
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: "select_account" });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
